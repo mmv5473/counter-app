e30526d35a45a66d06578e24bc966e560911dd1f
Marcos Vazquez <mmv5473@psu.edu>
2026-02-23 05:26:15 -0500

    Initial commit

---
+++ counter-app.js
@@ -0,0 +1,152 @@
+/**
+ * Copyright 2026 Marcos Vazquez
+ * @license Apache-2.0, see LICENSE for full text.
+ */
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+
+/**
+ * `counter-app`
+ * 
+ * @demo index.html
+ * @element counter-app
+ */
+export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
+
+  static get tag() {
+    return "counter-app";
+  }
+
+  constructor() {
+    super();
+    this.title = "";
+    this.count = 0;
+    this.min = 0;
+    this.max = 100;
+    this.t = this.t || {};
+    this.t = {
+      ...this.t,
+      title: "Title",
+    };
+    this.registerLocalization({
+      context: this,
+      localesPath:
+        new URL("./locales/counter-app.ar.json", import.meta.url).href +
+        "/../",
+    });
+  }
+
+  // Lit reactive properties
+  static get properties() {
+    return {
+      ...super.properties,
+      title: { type: String },
+      count: { type: Number, reflect: true },
+      min: { type: Number },
+      max: { type: Number },
+    };
+  }
+
+  // Lit scoped styles
+  static get styles() {
+    return [super.styles,
+    css`
+      :host {
+        display: block;
+        color: var(--ddd-theme-primary);
+        background-color: var(--ddd-theme-accent);
+        font-family: var(--ddd-font-navigation);
+      }
+      /* sets color when counter equals a certain number */
+      :host([count="18"]) h3 {
+        color: var(--ddd-theme-default-forestGreen);
+      }
+      :host([count="21"]) h3 {
+        color: var(--ddd-theme-default-roarGolden);
+      }
+      .wrapper {
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
+      }
+      h3 {
+        font-size: var(--ddd-font-size-3xl);
+      }
+      h3 span {
+        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+      }
+      h3.min,
+      h3.max {
+        color: var(--counter-app-min-color, var(--ddd-theme-default-discoveryCoral));
+      }
+      .btn {
+        margin: var(--ddd-spacing-1);
+      }
+    `];
+  }
+
+  // increment function when '+' button is clicked
+  increment() {
+    this.count++;
+  };
+  // decrement function when '-' button is clicked
+  decrement() {
+    this.count--;
+  }
+
+// function that is called whenever  a property changes (number incremented or decremented)
+updated(changedProperties) {
+  if (super.updated) {
+    super.updated(changedProperties);
+  }
+  if (changedProperties.has('count')) {
+    // do your testing of the value and make it rain by calling makeItRain
+    if (this.count == 21) {
+      this.makeItRain();
+    }
+  }
+}
+
+makeItRain() {
+  // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+  // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+  // will only run AFTER the code is imported and available to us
+  import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+    (module) => {
+      // This is a minor timing 'hack'. We know the code library above will import prior to this running
+      // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+      // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+      // it's listening for changes so it can react
+      setTimeout(() => {
+        // forcibly set the poppped attribute on something with id confetti
+        // while I've said in general NOT to do this, the confetti container element will reset this
+        // after the animation runs so it's a simple way to generate the effect over and over again
+        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+      }, 0);
+    }
+  );
+}
+
+  // Lit render the HTML
+  render() {
+    return html`
+    <confetti-container id="confetti">
+      <div class="wrapper">
+      <!-- disable button when count reaches min or max, otherwise do nothing -->
+      <h3 class="${this.count === this.min ? 'min' : this.count === this.max ? 'max' : ''}">${this.count}</h3>
+      <button class="btn" @click="${this.decrement}" ?disabled="${this.min === this.count}">-</button>
+      <button class="btn" @click="${this.increment}" ?disabled="${this.max === this.count}">+</button>
+      <slot></slot>
+      </div>
+    </confetti-container>
+    `;
+  }
+
+  /* haxProperties integration via file reference */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+      .href;
+  }
+}
+
+globalThis.customElements.define(CounterApp.tag, CounterApp);
